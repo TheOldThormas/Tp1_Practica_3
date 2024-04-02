1e131d0082ba4256345c6ae871f7ff8fd8e0a1f1
juliangaldamez <juliangaldamez0@gmail.com>
2024-04-02 00:43:09 -0300

    Agregue los cambios que hicimos con el valentin e hice funcional el html

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ app.config['MYSQL_PASSWORD']='123456789'
 app.config['MYSQL_DB']='imagenes'
 conexion=MySQL(app)
 
-@app.route('/')
+@app.route('/cargar')
 def productos():
-    return render_template('index.html')
+    return render_template('formulario.html')
 
 @app.route('/subir', methods=['POST'])
 def subir_archivo():
@@ -22,7 +22,13 @@ def subir_archivo():
     imagen = request.files['archivo']
     if imagen.filename == '':
         return 'ningún archivo'
-    if imagen: #Acordarse de poner los .title en cada captura del formulario para que queden los nombres con mayusculas
+    if imagen: 
+        producto=request.form.get('producto')
+        producto=producto.title()
+        precio=request.form.get('precio')
+        descripcion=request.form.get('descripcion')
+        descripcion=descripcion.title()
+        cantidad=request.form.get('cantidad')
         nombre_archivo=secure_filename(imagen.filename)
         tipo_archivo=nombre_archivo.rsplit('.', 1)[1]
         nombre_archivo=nombre_archivo.rsplit('.', 1)[0]
@@ -31,14 +37,14 @@ def subir_archivo():
         imagen.save('static/'+nombre_final)
         try:
             cursor=conexion.connection.cursor()
-            sql=f"INSERT INTO `producto` (`imagen_producto`) VALUES ('{nombre_final}');" #La consulta necesita que se le agregue el resto de columnas(nombre ,desc ,etc)
+            sql=f"INSERT INTO `producto` (`nombre_producto`, `precio_producto`, `descripcion_producto`, `cantidad_producto`, `imagen_producto`) VALUES ('{producto}', '{precio}', '{descripcion}', '{cantidad}', '{nombre_final}');"
             cursor.execute(sql)
             conexion.connection.commit()
         except Exception as e:
             print("Error MySQL:", str(e))
-        return redirect('/imagenes')
+        return redirect('/')
     
-@app.route("/imagenes")
+@app.route("/")
 def imagenes():
     try:
         cursor=conexion.connection.cursor()
@@ -47,7 +53,7 @@ def imagenes():
         tabla=cursor.fetchall()
     except Exception as e:
         print("Error MySQL:", str(e))
-    return render_template('imagenes.html',tabla=tabla)
+    return render_template('index.html',tabla=tabla)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)

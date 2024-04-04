2635cdf01c977689c8ac4a867f13f664dc7fe90d
cgabriel122 <164580968+cgabriel122@users.noreply.github.com>
2024-04-03 22:51:01 -0300

    Add files via upload

---
+++ Tp1_Practica_3-main/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+from werkzeug.utils import secure_filename
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='julian'
+app.config['MYSQL_PASSWORD']='123456789'
+app.config['MYSQL_DB']='imagenes'
+conexion=MySQL(app)
+
+@app.route('/cargar')
+def productos():
+    return render_template('formulario.html')
+
+@app.route('/subir', methods=['POST'])
+def subir_archivo():
+    if 'archivo' not in request.files:
+        return 'ningún archivo'
+    imagen = request.files['archivo']
+    if imagen.filename == '':
+        return 'ningún archivo'
+    if imagen:
+        producto=request.form.get('producto')
+        producto=producto.title()
+        precio=request.form.get('precio')
+        descripcion=request.form.get('descripcion')
+        descripcion=descripcion.title()
+        cantidad=request.form.get('cantidad')
+        nombre_archivo=secure_filename(imagen.filename)
+        tipo_archivo=nombre_archivo.rsplit('.', 1)[1]
+        nombre_archivo=nombre_archivo.rsplit('.', 1)[0]
+        nombre_modificado=f"{datetime.now().strftime('%Y%m%d%H%M%S')}.{tipo_archivo}"
+        nombre_final=nombre_archivo+nombre_modificado
+        imagen.save('static/'+nombre_final)
+        try:
+            cursor=conexion.connection.cursor()
+            sql=f"INSERT INTO `producto` (`nombre_producto`, `precio_producto`, `descripcion_producto`, `cantidad_producto`, `imagen_producto`) VALUES ('{producto}', '{precio}', '{descripcion}', '{cantidad}', '{nombre_final}');"
+            cursor.execute(sql)
+            conexion.connection.commit()
+        except Exception as e:
+            print("Error MySQL:", str(e))
+        return redirect('/')
+    
+@app.route("/")
+def imagenes():
+    try:
+        cursor=conexion.connection.cursor()
+        sql="SELECT * FROM producto"
+        cursor.execute(sql)
+        tabla=cursor.fetchall()
+    except Exception as e:
+        print("Error MySQL:", str(e))
+    return render_template('index.html',tabla=tabla)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)

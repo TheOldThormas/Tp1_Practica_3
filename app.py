6c7a93e4db3f25eee832eaa5f700f8230f05fcc2
TheOldThormas <1tomilopez@gmail.com>
2024-04-06 20:08:12 -0300

    mejorado del commit anterior

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 
@@ -75,28 +76,64 @@ def imagenes():
     return render_template('index.html', tabla=tabla)
 
 
-@app.route('/eliminar/<int:producto_id>', methods=['POST', 'GET'])
-def eliminar_producto(producto_id):
+@app.route('/modificar', methods=['GET', 'POST'])
+def modificar():
+    id_producto = request.args.get('id_producto')
     try:
-        print(producto_id)
         cursor = conexion.connection.cursor()
-        sql = f"SELECT imagen_producto FROM producto WHERE idproducto = {producto_id}"
+        sql = f"SELECT * FROM producto where idproducto = {id_producto}"
         cursor.execute(sql)
-        resultado = cursor.fetchone()
-        if resultado:
-            nombre_imagen = resultado[0]
-            sql = f"DELETE FROM producto WHERE idproducto = {producto_id}"
-            cursor.execute(sql)
-            conexion.connection.commit()
-            # Eliminar la imagen del servidor
-            import os
-            os.remove(os.path.join('static', nombre_imagen))
-            return redirect('/')
-        else:
-            return 'Producto no encontrado'
+        tabla = cursor.fetchone()
     except Exception as e:
         print("Error MySQL:", str(e))
-        return 'Error al eliminar el producto'
+    return render_template('modificar.html', tabla=tabla)
+
+
+@app.route('/confirmar', methods=['POST'])
+def confirmar():
+    id = request.form.get('id_producto')
+    imagen = request.files['archivo']
+    if imagen.filename == '':
+        nombre_final = request.form.get('imagen_actual')
+    else:
+        nombre_archivo = secure_filename(imagen.filename)
+        tipo_archivo = nombre_archivo.rsplit('.', 1)[1]
+        nombre_archivo = nombre_archivo.rsplit('.', 1)[0]
+        nombre_modificado = f"{datetime.now().strftime('%Y%m%d%H%M%S')}.{tipo_archivo}"
+        nombre_final = nombre_archivo+nombre_modificado
+        imagen_vieja = request.form.get('imagen_actual')
+        ubicacionvieja = os.path.join("static", imagen_vieja)
+        os.remove(ubicacionvieja)
+        imagen.save('static/'+nombre_final)
+    producto = request.form.get('producto')
+    producto = producto.title()
+    precio = request.form.get('precio')
+    descripcion = request.form.get('descripcion')
+    descripcion = descripcion.title()
+    cantidad = request.form.get('cantidad')
+    try:
+        cursor = conexion.connection.cursor()
+        sql = f"UPDATE `imagenes`.`producto` SET `precio_producto` = '{precio}', `nombre_producto` = '{producto}', `descripcion_producto` = '{descripcion}',  `cantidad_producto` = '{cantidad}', `imagen_producto` = '{nombre_final}'  WHERE (`idproducto` = '{id}');"
+        cursor.execute(sql)
+        conexion.connection.commit()
+    except Exception as e:
+        print("Error MySQL:", str(e))
+    return redirect('/')
+
+
+@app.route("/eliminar", methods=['GET', 'POST'])
+def eliminar():
+    id_producto = request.args.get('id_producto')
+    cursor = conexion.connection.cursor()
+    sql = f"SELECT imagen_producto FROM producto WHERE idproducto = {id_producto}"
+    cursor.execute(sql)
+    resultado = cursor.fetchone()
+    nombre_imagen = resultado[0]
+    sql = f"DELETE FROM producto WHERE idproducto = {id_producto}"
+    cursor.execute(sql)
+    conexion.connection.commit()
+    os.remove(os.path.join('static', nombre_imagen))
+    return redirect('/')
 
 
 if __name__ == '__main__':

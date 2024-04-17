ca53b8a69e545766a153dd2e9474ad3334872b92
juliangaldamez <juliangaldamez0@gmail.com>
2024-04-17 03:34:17 -0300

    borre duplicado del merge

--- app.py
+++ app.py
@@ -10,9 +10,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY']='12345'
 app.config['SESSION_TYPE']='filesystem'
 Session(app)
-app.config['SECRET_KEY']='12345'
-app.config['SESSION_TYPE']='filesystem'
-Session(app)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'julian'
